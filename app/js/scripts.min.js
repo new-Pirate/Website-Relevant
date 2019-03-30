8607b41bf369ffa362abf9a4559f28ae5280b3e0
Alex <PR1ME-GOP@yandex.ru>
2019-03-30 15:43:36 +0400

    Сверстал блок формы и всплывающий popup

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -4,4 +4,28 @@ $(document).ready(function () {
         items: 1,
         nav: true,
     });
+
+    /* Валидация для телефонов */
+
+    $("#tel, #popupTel").mask("+7(999) 999-99-99");
+});
+
+
+/* Popup */
+
+function PopUpShow() {
+    $("#popup").fadeIn(500).show();
+}
+
+function PopUpHide() {
+    $("#popup").hide();
+}
+
+$("#callbackPopup").click(function () {
+    PopUpShow();
+});
+
+$("#closePopup, #sendButton").click(function () {
+    PopUpHide();
+    return false;
 });
