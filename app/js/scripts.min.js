e080f25f86c5380413d0556d90297eed2d68e4da
Alex <PR1ME-GOP@yandex.ru>
2019-04-01 01:55:49 +0400

    Добавил анимацию и закинул задание

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -8,6 +8,63 @@ $(document).ready(function () {
     /* Валидация для телефонов */
 
     $("#tel, #popupTel").mask("+7(999) 999-99-99");
+
+    /* Animate.css */
+
+    $('.mainTask').viewportChecker({
+        classToAdd: 'vision animated zoomIn',
+        offset: 50
+    });
+
+    $('.task_title').viewportChecker({
+        classToAdd: 'vision animated fadeInUp',
+        offset: 10
+    });
+
+    $('.task_client').viewportChecker({
+        classToAdd: 'vision animated fadeInRight',
+        offset: 50
+    });
+
+    $('.task_data').viewportChecker({
+        classToAdd: 'vision animated fadeInLeft',
+        offset: 50
+    });
+
+    $('.task_tasks').viewportChecker({
+        classToAdd: 'vision animated fadeInUp',
+        offset: 50
+    });
+
+    $('.made_title').viewportChecker({
+        classToAdd: 'vision animated zoomInLeft',
+        offset: 20
+    });
+
+    $('.made_block').viewportChecker({
+        classToAdd: 'vision animated fadeInLeft',
+        offset: 10
+    });
+
+    $('.result_content').viewportChecker({
+        classToAdd: 'vision animated fadeInRight',
+        offset: 20
+    });
+
+    $('.result_slider').viewportChecker({
+        classToAdd: 'vision animated fadeInUp',
+        offset: 20
+    });
+
+    $('.form').viewportChecker({
+        classToAdd: 'vision animated fadeInLeft',
+        offset: 10
+    });
+
+    $('.callback').viewportChecker({
+        classToAdd: 'vision animated fadeInRight',
+        offset: 10
+    });
 });
 
 
